c7218d23cc62b0460762e4a203a886b565f4fe5e
CRUXsur <marcojtorrico@hotmail.com>
2022-02-14 11:11:56 -0400

    Finalizar el MovieSlider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,8 +7,8 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity, //
-      height: 250, //
-      color: Colors.red,
+      height: 260, //
+      //color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -20,8 +20,9 @@ class MovieSlider extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          const SizedBox(height: 5),
           //! Genera un error por el tamano del ListView y Column
-          //!=> el ListView.builder lo wrap en eun expanded!
+          //!=> el ListView.builder lo wrap en eun Expanded!
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -41,8 +42,35 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      //color: Colors.green,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          //imagen del poster
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          //texto de la pelicul;a
+          const Text(
+            'StarWars: El retorno de el nuevo Jedi de Monte Cristo',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
